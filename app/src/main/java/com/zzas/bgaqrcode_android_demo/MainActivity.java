c22779b31c7ab145816812227b1ad096a4a585e4
qxinzheng <312782144.qq.com>
2019-08-21 16:20:25 +0800

    完成Zxing扫码功能

--- app/src/main/java/com/zzas/bgaqrcode_android_demo/MainActivity.java
+++ app/src/main/java/com/zzas/bgaqrcode_android_demo/MainActivity.java
@@ -1,13 +1,62 @@
 package com.zzas.bgaqrcode_android_demo;
 
+import android.Manifest;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+    private static final int REQUEST_CODE_QRCODE_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnZxing:
+                startActivity(new Intent(this, ZxingTestScanActivity.class));
+                break;
+            case R.id.btnZbar:
+                startActivity(new Intent(this, ZxingTestScanActivity.class));
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        requestCodeQRCodePermissions();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
+
+    @AfterPermissionGranted(REQUEST_CODE_QRCODE_PERMISSIONS)
+    private void requestCodeQRCodePermissions() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (!EasyPermissions.hasPermissions(this, perms)) {
+            EasyPermissions.requestPermissions(this, "扫描二维码需要打开相机和散光灯的权限", REQUEST_CODE_QRCODE_PERMISSIONS, perms);
+        }
     }
 }
---
+++ app/src/main/java/com/zzas/bgaqrcode_android_demo/ZxingTestScanActivity.java
@@ -0,0 +1,83 @@
+package com.zzas.bgaqrcode_android_demo;
+
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zxing.ZXingView;
+
+public class ZxingTestScanActivity extends AppCompatActivity implements QRCodeView.Delegate{
+    private static final String TAG = ZxingTestScanActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY = 666;
+
+    private ZXingView mZXingView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zxing_test_scan);
+
+        mZXingView = findViewById(R.id.zxingview);
+        mZXingView.setDelegate(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mZXingView.startCamera(); // 打开后置摄像头开始预览，但是并未开始识别
+//        mZXingView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT); // 打开前置摄像头开始预览，但是并未开始识别
+
+        mZXingView.startSpotAndShowRect(); // 显示扫描框，并开始识别
+    }
+
+    @Override
+    protected void onStop() {
+        mZXingView.stopCamera(); // 关闭摄像头预览，并且隐藏扫描框
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mZXingView.onDestroy(); // 销毁二维码扫描控件
+        super.onDestroy();
+    }
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
+    }
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        Log.i(TAG, "result:" + result);
+        setTitle("扫描结果为：" + result);
+        vibrate();
+
+        mZXingView.startSpot(); // 开始识别
+    }
+
+    @Override
+    public void onCameraAmbientBrightnessChanged(boolean isDark) {
+        // 这里是通过修改提示文案来展示环境是否过暗的状态，接入方也可以根据 isDark 的值来实现其他交互效果
+        String tipText = mZXingView.getScanBoxView().getTipText();
+        String ambientBrightnessTip = "\n环境过暗，请打开闪光灯";
+        if (isDark) {
+            if (!tipText.contains(ambientBrightnessTip)) {
+                mZXingView.getScanBoxView().setTipText(tipText + ambientBrightnessTip);
+            }
+        } else {
+            if (tipText.contains(ambientBrightnessTip)) {
+                tipText = tipText.substring(0, tipText.indexOf(ambientBrightnessTip));
+                mZXingView.getScanBoxView().setTipText(tipText);
+            }
+        }
+    }
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        Log.e(TAG, "打开相机出错");
+    }
+}
